c584475dba6d6bb075b88843564d115bd547bf37
Jonas Dåderman <jonasdad@kth.se>
2023-01-24 08:43:48 +0100

    Fixed bugs

--- environment.ex
+++ environment.ex
@@ -1,18 +1,17 @@
 defmodule EnvList do
-  def new() do list = [] end
-  #Adds key-value pair at start of list O(1) complexity
-  def add(map, key, value) do map = [{key, value}] ++ map end
-  #Adds key-value pair at end of list O(n) complexity
-  def add2(map, key, value) do map = map ++ [{key, value}] end
-  def lookup(map, key) do map[key] end
-  def remove(key, map) do map = {key, map[key]} -- map end
 
-  def test() do
-    test_list = EnvList.new()
-    EnvList.add(test_list, :a, 1)
-    EnvList.add(test_list, :b, 2)
-    EnvList.lookup(test_list, b)
-#    EnvList.remove(:b, test_list)
+  #Return empty list
+  def new() do [] end
+  #Add - 3 cases - Empty, Key found -> update value, Key not found insert
+  def add([], key, value) do [{key, value}] end #if list is empty -> do {key, value}
+  def add([h | t], key, value) do [h | add(t, key, value)] end #Traverse list recursively
+  def add([{key, _}| t], key ,value) do [{key, value} | t] end #update key
 
-  end
+  def lookup([], _) do nil end #key not present
+  def lookup([{key, _} | t ], key) do {key, value} end #key found -> return key-value pair
+  def lookup([_ | t], key) do lookup(t, key) end
+
+  def remove([], _) do [] end #Remove from empty list -> Empty list, []
+  def remove([{key, value} | t], key) do t end
+  def remove([h | t], key) do [h | remove(t, key)]end
 end
