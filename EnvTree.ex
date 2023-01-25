3e93b7a52098c978bd7a936780de48397f674a5e
Jonasdad <Jonasdad@kth.se>
2023-01-25 11:57:19 +0100

    Tree Lookup, Parts of Remove

--- EnvTree.ex
+++ EnvTree.ex
@@ -11,7 +11,7 @@ defmodule EnvTree do
     test_tree = EnvTree.add(test_tree, :a, 5)
     test_tree = EnvTree.add(test_tree, :c, 2)
     test_tree = EnvTree.add(test_tree, :a, 3)
-    lookup(test_tree, :c)
+    #lookup(test_tree, :c)
     #test_tree = EnvTree.add(test_tree, :d, 6)
     #test_tree = EnvTree.add(test_tree, :e, 7)
 
@@ -31,16 +31,28 @@ defmodule EnvTree do
   def add({:node, k, v, left, right}, key, value) when key < k do
       {:node, k, v, add(left, key, value), right}
     end
-
   #same thing but instead update the right banch
   def add({:node, k, v, left, right}, key, value) do
     {:node, k,v,left,add(right,key,value)}
     end
 
+  #Element not found and cannot be found
   def lookup({:nil, k, _}, key) do nil end
   def lookup({:node, key, value, _, _}, key) do {key, value} end
   def lookup({:node, k, _, left, _}, key) when key < k do lookup(left, key) end
-  def lookup({:node, k, _, _, right}, key) do {:node, k, value, left, lookup(right, key)} end
+  def lookup({:node, k, _, _, right}, key) do lookup(right, key) end
 
+  #Element not found and cannot be removed
+  def remove(nil, _) do nil end
 
+  def remove({:node, key, _, nil, right}, key) do right end
+  def remove({:node, key, _, left, nil}, key) do left end
+  def remove({:node, key, _, left, right}, key) do {key, value, rest} = leftmost(right); {:node, , ..., ..., ...} end
+
+  def remove({:node, k, v, left, right}, key) when key < k do {:node, k, v, remove(left, key), right} end
+  def remove({:node, k, v, left, right}, key) do {:node, k, v, left, remove(right, key)} end
+
+  def leftmost({:node, key, value, nil, rest}) do rest end
+  def leftmost({:node, k, v, left, right}) do {:node, k, v} = leftmost(left) end
+  end
 end
