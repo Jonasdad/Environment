c9a387cd07412d11d0b7d446e32025cbdea56a80
Jonasdad <Jonasdad@kth.se>
2023-01-25 17:21:24 +0100

    Working tree (i think?)

--- EnvTree.ex
+++ EnvTree.ex
@@ -11,6 +11,7 @@ defmodule EnvTree do
     test_tree = EnvTree.add(test_tree, :a, 5)
     test_tree = EnvTree.add(test_tree, :c, 2)
     test_tree = EnvTree.add(test_tree, :a, 3)
+    test_tree = EnvTree.remove(test_tree, :a)
     #lookup(test_tree, :c)
     #test_tree = EnvTree.add(test_tree, :d, 6)
     #test_tree = EnvTree.add(test_tree, :e, 7)
@@ -28,13 +29,9 @@ defmodule EnvTree do
 
   #return a tree that looks like the one we have
   #but where the left branch has been updated ...
-  def add({:node, k, v, left, right}, key, value) when key < k do
-      {:node, k, v, add(left, key, value), right}
-    end
+  def add({:node, k, v, left, right}, key, value) when key < k do {:node, k, v, add(left, key, value), right} end
   #same thing but instead update the right banch
-  def add({:node, k, v, left, right}, key, value) do
-    {:node, k,v,left,add(right,key,value)}
-    end
+  def add({:node, k, v, left, right}, key, value) do {:node, k,v,left,add(right,key,value)} end
 
   #Element not found and cannot be found
   def lookup({:nil, k, _}, key) do nil end
@@ -44,15 +41,13 @@ defmodule EnvTree do
 
   #Element not found and cannot be removed
   def remove(nil, _) do nil end
-
   def remove({:node, key, _, nil, right}, key) do right end
   def remove({:node, key, _, left, nil}, key) do left end
-  def remove({:node, key, _, left, right}, key) do {key, value, rest} = leftmost(right); {:node, , ..., ..., ...} end
+  def remove({:node, key, _, left, right}, key) do {key, value, remainder} = leftmost(right); {:node, key, value, left, remainder} end
 
   def remove({:node, k, v, left, right}, key) when key < k do {:node, k, v, remove(left, key), right} end
   def remove({:node, k, v, left, right}, key) do {:node, k, v, left, remove(right, key)} end
 
   def leftmost({:node, key, value, nil, rest}) do rest end
   def leftmost({:node, k, v, left, right}) do {:node, k, v} = leftmost(left) end
-  end
 end
